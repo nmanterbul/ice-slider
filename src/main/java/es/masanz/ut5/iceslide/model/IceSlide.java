6d2e5641b0a4ddf5f73c4b9646c31a60fbcaaaa6
nmanterbul <nmanterbul@educacion.navarra.es>
2026-01-07 12:56:29 +0100

    Add files via upload

--- src/main/java/es/masanz/ut5/iceslide/model/IceSlide.java
+++ src/main/java/es/masanz/ut5/iceslide/model/IceSlide.java
@@ -6,7 +6,7 @@ public class IceSlide {
 
     // TODO 01: Definicion de atributos
 
-    private int curLevel;
+    private Nivel curLevel;
     private int[][] escenario;
     private int filaJugador, columnaJugador;
 
@@ -14,30 +14,56 @@ public class IceSlide {
         // TODO 02: conscructor 1
        this.columnaJugador = altura;
        this.filaJugador = anchura;
+
+
     }
 
     public IceSlide(int[][] escenario){
         // TODO 03: conscructor 2
+       clonarEscenario(escenario);
+       int filas = escenario.length;
+       int columnas = escenario[0].length;
 
-        if(filaJugador == 0 && columnaJugador == 0){
-            new IceSlide(filaJugador, columnaJugador);
-        }else {
-            clonarEscenario(escenario);
 
+        boolean encontrado = false;
+
+        for (int i = 0; i <filas && !encontrado; i++) {
+            for (int j = 0; j < columnas && !encontrado; j++) {
+                /*
+                if(this.escenario[i][j] == Celda.JUGADOR){
+                   filaJugador = i;
+                   columnaJugador = j;
+                   encontrado = true;
+                }
+                
+                 */
+            }
         }
+
+        if (!encontrado){
+            filaJugador = 0;
+            columnaJugador = 0;
+        }
+
 
     }
 
     private void clonarEscenario(int[][] escenario) {
         // TODO 04: clonar la matriz facilitada sobre el atributo escenario
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < columnaJugador; j++) {
 
-                for (int k = 0; k < filaJugador; k++) {
-                   int a =  escenario[j][k];
+        filaJugador = escenario.length;
+        columnaJugador  = escenario[0].length;
+
+        int[] [] clonar = new int[filaJugador][columnaJugador];
+
+
+            for (int j = 0; j < filaJugador; j++) {
+
+                for (int k = 0; k < columnaJugador; k++) {
+                   clonar[j][k] =  escenario[j][k];
                 }
             }
-        }
+
 
     }
 
@@ -63,19 +89,57 @@ public class IceSlide {
     public void moverJugador(int despFila, int despCol){
         // TODO 06: desplazar el jugador siguiendo las reglas indicadas
 
-        for (int i = 0; i < escenario.length; i++) {
-//            if( Celda.VACIO && Celda.HIELO &&  despFila == filaJugador && despCol == columnaJugador){
-//                despFila++;
-//                despCol++;
-//            }
+        int nuevaFila = filaJugador + despFila;
+        int nuevaCol = columnaJugador + despCol;
+
+        if (nuevaFila < 0 || nuevaFila >= escenario.length || nuevaCol < 0 || nuevaCol >= escenario[0].length){
+            return;
         }
+
+
+
+        for (int i = 0; i < escenario.length; i++) {
+
+            if ( VACIO!= null && HIELO!=null){
+                /*
+                desplazarHielo(despFila,despCol,);
+
+                 */
+            }
+        }
+
 
 
     }
 
     private boolean desplazarHielo(int filaHielo, int colHielo, int despFila, int despCol) {
         // TODO 07: desplazar el bloque de hielo siguiendo las reglas indicadas
+
+        for (int i = 0; i <colHielo ; i++) {
+            for (int j = 0; j < filaHielo; j++) {
+                if(Celda.VACIO != null){
+                    moverJugador(despFila,despCol);
+                    return true;
+
+                }else if (FUEGO != null){
+                    /*
+                    CHARCO;
+
+                     */
+                }
+
+
+            }
+
+
+
+        }
+
+
+
         return false;
+
+
     }
 
     public boolean completado(){
